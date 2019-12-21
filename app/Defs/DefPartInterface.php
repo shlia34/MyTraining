8c6f5aab4763236cd80747762a2ce4198c122041
白川聡 <kyabetsu3104@gmail.com>
2019-12-22 03:54:39 +0900

    定義ファイル変更

---
+++ app/Defs/DefPartInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Defs;
+
+interface DefPartInterface
+{
+    const MUNE_PART_CODE = "01";
+    const MUNE_PART_NAME = "胸";
+
+    const SENAKA_PART_CODE = "02";
+    const SENAKA_PART_NAME = "背中";
+
+    const ASHI_PART_CODE = "03";
+    const ASHI_PART_NAME = "脚";
+
+    const KATA_PART_CODE = "04";
+    const KATA_PART_NAME = "肩";
+
+    const NITOU_PART_CODE = "05";
+    const NITOU_PART_NAME = "二頭筋";
+
+    const SANTOU_PART_CODE = "06";
+    const SANTOU_PART_NAME = "三頭筋";
+
+    const HARA_PART_CODE = "07";
+    const HARA_PART_NAME = "腹筋";
+}
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Defs\DefPart;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -14,4 +15,11 @@ class Controller extends BaseController
     public function __construct(){
         $this->middleware('auth');
     }
+
+    //todo uuid生成をみ直す。衝突の可能性が高い？
+    protected function generateId($prefix){
+        $uuid = md5(uniqid());
+        return $prefix . $uuid;
+    }
+
 }
