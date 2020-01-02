3b04f804a55175973e4255c8c8eb4c16511fa011
白川聡 <kyabetsu3104@gmail.com>
2020-01-02 12:57:28 +0900

    コントローラーを綺麗に

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 
+use App\Event;
+use App\Training;
+
 class TopController extends Controller
 {
 
     public function index()
     {
         return view('top.index');
+    }
+
+    public function show($eventId){
+        $event = Event::find($eventId);
+        $trainings = Training::where('event_id', $eventId)->orderBY("created_at")->get();
+
+        return view('event.show')->with(['event' => $event,'trainings'=>$trainings]);
     }
 }
--- app/Training.php
+++ app/Training.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Defs\DefStage;
 use Illuminate\Database\Eloquent\Model;
 
 class Training extends Model
@@ -9,5 +10,9 @@ class Training extends Model
     protected $primaryKey = 'training_id';
     public $incrementing = false;
     protected $keyType = 'string';
+
+    public function getStageName(){
+        return array_column(DefStage::STAGE_LIST, $this->stage_code);
+    }
 
 }
