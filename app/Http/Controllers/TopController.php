ed657555062f62c32338ef0018d35fbc342f0e65
白川聡 <kyabetsu3104@gmail.com>
2020-01-21 14:15:58 +0900

    トレーニングを種目別に表示(サーバー側)

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -14,10 +14,14 @@ class TopController extends Controller
         return view('top.index');
     }
 
+    //todo 404処理(そのページはもうないです的な)
+    //todo 「カレンダーに戻る」ボタンつける
     public function show($eventId){
         $event = Event::find($eventId);
-        $trainings = Training::where('event_id', $eventId)->orderBY("created_at")->get();
-
+        $trainings = Training::where('event_id', $eventId)->orderBY("created_at")->get()->groupBy('stage_code');
         return view('event.show')->with(['event' => $event,'trainings'=>$trainings]);
     }
+    //todo chart.jsかなんかでグラフを使用
+    //todo タイマーとかもあったら良い
+    //todo 筋トレ種目の説明とかのページもあったら良い。コンパウンド、トレーニングのいつやるべき種目なのか
 }
