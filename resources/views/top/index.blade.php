1cdaa1e2048387ff4564e05748335d24121f6f3f
shira <shira79@users.noreply.github.com>
2020-01-27 02:12:46 +0900

    モバイルとPCで表示するデータを少し変更

--- resources/views/top/index.blade.php
+++ resources/views/top/index.blade.php
@@ -14,7 +14,7 @@
 
     <div class="remodal" data-remodal-id="modal" data-remodal-options="hashTracking:false">
         <button data-remodal-action="close" class="remodal-close"></button>
-        {{Form::select('part_code', App\Defs\DefPart::PART_LIST,null,['class' => 'remodal-part_code'])}}
+        {{Form::select('part_code', App\Defs\DefPart::PART_NAME_LIST,null,['class' => 'remodal-part_code'])}}
         {{Form::input('text', 'memo',null,['class' => 'remodal-memo'])}}
         <button data-remodal-action="cancel" class="remodal-cancel">Cancel</button>
         <button data-remodal-action="confirm" class="remodal-confirm">OK</button>
