84949e5f64e7f9b3fa867aa633771051d573afcc
shira <shira79@users.noreply.github.com>
2020-02-03 01:55:44 +0900

    UI調整

--- resources/views/top/show.blade.php
+++ resources/views/top/show.blade.php
@@ -3,26 +3,33 @@
 
 <div class = "wrapper">
 
+    <div class = "add-training-form-box form-inline pt-3 pl-3 pr-3">
+        {{ Form::select('stage_code', App\Defs\DefStage::STAGE_NAME_LIST[$thisEvent->part_code],null ,['class' => 'form-stage_code browser-default custom-select mb-2']) }}
+        <div class="forms">
+            <div class="md-form m-0 ml-3 w-25">
+                <input type="number" id="form-weight" class="form-weight form-control">
+                <label for="form-weight">weight</label>
+            </div>
 
-    {{ Form::select('stage_code', App\Defs\DefStage::STAGE_NAME_LIST[$thisEvent->part_code],null ,['class' => 'form-stage_code']) }}
-    <div>
-        {{Form::input('number', 'weight',null,['class' => 'form-weight'])}}kg *
-        {{ Form::selectRange('rep', 1, 35, null, ['class' => 'form-rep']) }}rep
-
-        <button class = "add-training-btn _transparent" type="button"><i class="fas fa-plus-circle"></i>追加</button>
+            <div class="md-form m-0 ml-3 w-25">
+                <input type="number" id="form-rep" class="form-rep form-control">
+                <label for="form-rep">rep</label>
+            </div>
+            <button class = "add-training-btn _transparent btn btn-dark waves-effect w-30 ml-4" type="button">追加</button>
+        </div>
     </div>
 
-    <div class = "trainings-index">
+    <div class = "trainings-index special-color-dark">
 
         <div class = "today-trainings">
-            <div class = "event-title">今回の{{App\Defs\DefPart::PART_NAME_LIST[$thisEvent->part_code]}}トレ {{$thisEvent->date}}</div>
+            <div class = "event-title">{{$thisEvent->date}}の{{App\Defs\DefPart::PART_NAME_LIST[$thisEvent->part_code]}}トレ</div>
             <div class="this-event-show" data-event_id= {{$thisEvent->event_id}}>
                 {{$thisEvent->getMemo()}}
                 <div class = "this-trainings">
                     @foreach($thisTrainings as $group)
                         <div class = "card mt-2 mb-2 mr-2 ml-2 p-2">
                             <span class = "card-title mb-0">{{ $group[0]->getStageName() }}</span>
-                            <ol data-stage_code = {{ $group[0]->stage_code }} class = "mb-0">
+                            <ol data-stage_code = {{ $group[0]->stage_code }} class = "ol-training mb-0">
                             @foreach($group as $training)
                                     @if($training->training_id == $thisEvent->max_training_id)
                                         <li data-training_id = {{$training->training_id}} class = "this-training _add-underline ">{{$training->getWeightAndRep() }}</li>
@@ -44,14 +51,14 @@
         @isset($lastEvent)
 
             <div class = "last-trainings">
-                <div class = "event-title">前回の{{App\Defs\DefPart::PART_NAME_LIST[$lastEvent->part_code]}}トレ {{$lastEvent->date}}</div>
+                <div class = "event-title">{{$lastEvent->date}}の{{App\Defs\DefPart::PART_NAME_LIST[$lastEvent->part_code]}}トレ</div>
                 <div class="this-event-show" data-event_id= {{$lastEvent->event_id}}>
                     {{$lastEvent->getMemo()}}
                     <div class = "last-trainings">
                         @foreach($lastTrainings as $group)
                             <div class = "card mt-2 mb-2 mr-2 ml-2 p-2">
                                 <span class = "card-title mb-0">{{ $group[0]->getStageName() }}</span>
-                                <ol data-stage_code = {{ $group[0]->stage_code }} class = "mb-0">
+                                <ol data-stage_code = {{ $group[0]->stage_code }} class = "ol-training mb-0">
                                 @foreach($group as $training)
                                     @if($training->training_id == $lastEvent->max_training_id)
                                         <li data-training_id = {{$training->training_id}} class = "last-training _add-underline ">{{$training->getWeightAndRep() }}</li>
