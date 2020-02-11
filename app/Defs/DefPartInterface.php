4fd5925331d6b30c025cfd10aac6d10c69983917
白川聡 <kyabetsu3104@gmail.com>
2020-02-11 14:01:54 +0900

    部位の配色変更

--- app/Defs/DefPartInterface.php
+++ app/Defs/DefPartInterface.php
@@ -17,7 +17,7 @@ interface DefPartInterface
     const MUNE_PART_CODE = "01";
     const MUNE_SHORT_PART_NAME = "胸";
     const MUNE_PART_NAME = "胸";
-    const MUNE_PART_COLOR = "#CC9933";
+    const MUNE_PART_COLOR = "#d50000";
 
     /**
      * 背中に関する定数
@@ -25,7 +25,8 @@ interface DefPartInterface
     const SENAKA_PART_CODE = "02";
     const SENAKA_SHORT_PART_NAME = "背";
     const SENAKA_PART_NAME = "背中";
-    const SENAKA_PART_COLOR = "#66CC00";
+//    const SENAKA_PART_COLOR = "#66CC00";
+    const SENAKA_PART_COLOR = "#104B2C";
 
     /**
      * 脚に関する定数
@@ -33,7 +34,8 @@ interface DefPartInterface
     const ASHI_PART_CODE = "03";
     const ASHI_SHORT_PART_NAME = "脚";
     const ASHI_PART_NAME = "脚";
-    const ASHI_PART_COLOR = "#000033";
+//    const ASHI_PART_COLOR = "#000033";
+    const ASHI_PART_COLOR = "#3A337F";
 
     /**
      * 肩に関する定数
@@ -41,7 +43,7 @@ interface DefPartInterface
     const KATA_PART_CODE = "04";
     const KATA_SHORT_PART_NAME = "肩";
     const KATA_PART_NAME = "肩";
-    const KATA_PART_COLOR = "#FF6666";
+    const KATA_PART_COLOR = "#ff4081";
 
     /**
      * 二頭筋に関する定数
@@ -49,7 +51,7 @@ interface DefPartInterface
     const NITOU_PART_CODE = "05";
     const NITOU_SHORT_PART_NAME = "二";
     const NITOU_PART_NAME = "二頭筋";
-    const NITOU_PART_COLOR = "#CCCC00";
+    const NITOU_PART_COLOR = "#57B760";
 
     /**
      * 三頭筋に関する定数
@@ -57,7 +59,7 @@ interface DefPartInterface
     const SANTOU_PART_CODE = "06";
     const SANTOU_SHORT_PART_NAME = "三";
     const SANTOU_PART_NAME = "三頭筋";
-    const SANTOU_PART_COLOR = "#FF5F17";
+    const SANTOU_PART_COLOR = "#ef6c00";
 
     /**
      * 腹筋に関する定数
@@ -65,6 +67,6 @@ interface DefPartInterface
     const HARA_PART_CODE = "07";
     const HARA_SHORT_PART_NAME = "腹";
     const HARA_PART_NAME = "腹筋";
-    const HARA_PART_COLOR = "#808080";
+    const HARA_PART_COLOR = "#454545";
 }
 
