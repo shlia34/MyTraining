0f172354ec7cb6071cad3f52653f63f3425091d1
shira <shira79@users.noreply.github.com>
2020-02-15 22:43:11 +0900

    csvからDB復元成功

--- app/Http/Controllers/CsvController.php
+++ app/Http/Controllers/CsvController.php
@@ -2,20 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use SplFileObject;
 
 class CsvController extends Controller
 {
 
     public function downloadTable($modelName){
 
+        $modelClass = "App\\".$modelName;
+
         $filename = $modelName."_".date('Ymd').".csv";
 
         $stream = fopen('php://temp', 'r+b');
 
         $output = array();
-        $columnList = $modelName::getCsvList();
+        $columnList = $modelClass::getCsvList();
 
         foreach($columnList as $column){
             $output[] = $column;
@@ -23,7 +23,7 @@ class CsvController extends Controller
         // CSVファイルを出力
         fputcsv($stream, $output);
 
-        $items = $modelName::all();
+        $items = $modelClass::all();
 
         foreach ($items as $items) {
             $output = array();
@@ -56,63 +56,11 @@ class CsvController extends Controller
 
     //todo この二つの名前は変更する
     public function TrainingDownload(){
-        return $this->downloadTable("App\Training");
+        return $this->downloadTable("Training");
     }
 
     public function EventDownload(){
-        return $this->downloadTable("App\Event");
+        return $this->downloadTable("Event");
     }
-
-    public function importCsv($request){
-
-
-        $originalName = $request->file('csv')->getClientOriginalName();
-
-        $tmpname = $originalName."_".uniqid("_").'csv'; //TMPファイル名
-
-        $csv = $request->file('csv');
-
-        $csv->move(public_path()."/csv/tmp",$tmpname);
-        $tmppath = public_path()."/csv/tmp/".$tmpname;
-
-        setlocale(LC_ALL, 'ja_JP.UTF-8');
-        file_put_contents($tmppath, mb_convert_encoding(file_get_contents($tmppath), 'UTF-8', 'SJIS'));
-
-        try {
-            $file = new SplFileObject($tmppath);
-            $file->setFlags(SplFileObject::READ_CSV);
-            $records = [];
-            $columnList = $file->fgetcsv();
-            //一行目、すなわちカラム行を配列として取得
-
-            foreach ($file as $line) {
-                if($file->key() > 0 && !$file->eof()){
-                    $record = [];
-                    for($i=0; $i < count($line); $i++){
-                        $record[ $columnList[$i] ] = $line[$i];
-                    }
-                    $records[] = $record;
-                }
-            }
-
-            return $records;
-
-        } catch (Exception $e) {
-            echo  $e->getMessage();
-        }finally{
-        }
-
-        return null;
-    }
-
-    public function importEvent(Request $request){
-        $record = $this->importCsv($request);
-        dd($record);
-    }
-
-
-
-    public function importTraining(){}
-
 
 }
