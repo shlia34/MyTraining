27773e56fa131f8eb04a5712d193fc895f8b5c67
shira <shira79@users.noreply.github.com>
2020-02-16 02:07:06 +0900

    DB修正

--- database/migrations/2019_12_11_144113_create_trainings_table.php
+++ database/migrations/2019_12_11_144113_create_trainings_table.php
@@ -17,8 +17,8 @@ class CreateTrainingsTable extends Migration
         Schema::create('trainings', function (Blueprint $table) {
             $table->string('training_id', 34)->primary();
             $table->string('event_id',34);
-            $table->foreign('event_id')->references('event_id')->on('events');
             $table->char('stage_code', 5);
+//            $table->string('stage_id', 34)->primary();
             $table->float('weight', 4, 1);
             $table->char('rep',2);
             $table->timestamps();
