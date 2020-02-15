45381a084577cdd9a2bb6b5a4153592d7afd56bf
shira <shira79@users.noreply.github.com>
2020-02-15 20:31:36 +0900

    csv一旦読み込めた

--- app/Http/Controllers/CsvController.php
+++ app/Http/Controllers/CsvController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use SplFileObject;
+
 class CsvController extends Controller
 {
 
-    public function EventDownload(){
-        return $this->tableDownload("App\Event");
-    }
-
-    public function tableDownload($modelName){
+    public function downloadTable($modelName){
 
         $filename = $modelName."_".date('Ymd').".csv";
 
@@ -55,8 +54,65 @@ class CsvController extends Controller
 
     }
 
+    //todo この二つの名前は変更する
     public function TrainingDownload(){
-        return $this->tableDownload("App\Training");
+        return $this->downloadTable("App\Training");
     }
+
+    public function EventDownload(){
+        return $this->downloadTable("App\Event");
+    }
+
+    public function importCsv($request){
+
+
+        $originalName = $request->file('csv')->getClientOriginalName();
+
+        $tmpname = $originalName."_".uniqid("_").'csv'; //TMPファイル名
+
+        $csv = $request->file('csv');
+
+        $csv->move(public_path()."/csv/tmp",$tmpname);
+        $tmppath = public_path()."/csv/tmp/".$tmpname;
+
+        setlocale(LC_ALL, 'ja_JP.UTF-8');
+        file_put_contents($tmppath, mb_convert_encoding(file_get_contents($tmppath), 'UTF-8', 'SJIS'));
+
+        try {
+            $file = new SplFileObject($tmppath);
+            $file->setFlags(SplFileObject::READ_CSV);
+            $records = [];
+            $columnList = $file->fgetcsv();
+            //一行目、すなわちカラム行を配列として取得
+
+            foreach ($file as $line) {
+                if($file->key() > 0 && !$file->eof()){
+                    $record = [];
+                    for($i=0; $i < count($line); $i++){
+                        $record[ $columnList[$i] ] = $line[$i];
+                    }
+                    $records[] = $record;
+                }
+            }
+
+            return $records;
+
+        } catch (Exception $e) {
+            echo  $e->getMessage();
+        }finally{
+        }
+
+        return null;
+    }
+
+    public function importEvent(Request $request){
+        $record = $this->importCsv($request);
+        dd($record);
+    }
+
+
+
+    public function importTraining(){}
+
 
 }
