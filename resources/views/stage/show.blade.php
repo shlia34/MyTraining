267f85b10bf5bc1f0840c9c88746fb75c52af39e
shira <shira79@users.noreply.github.com>
2020-02-28 21:42:20 +0900

    微調整

--- resources/views/stage/show.blade.php
+++ resources/views/stage/show.blade.php
@@ -7,9 +7,9 @@
             <div class = "">
                 @foreach($trainings as $eventId => $group)
                     <div class = "card mt-2 mb-2 mr-2 ml-2 p-2">
-                        <a href="/event/{{ $group[0]->event_id }}">
-                            <span class = "card-title mb-0">{{ $group[0]->date }}</span>
-                        </a>
+                        <span class = "mb-0">
+                            <a class = "card-title" href="/event/{{ $group[0]->event_id }}">{{ $group[0]->date }}</a>
+                        </span>
                         <ol data-event_id = {{$eventId}} class = "ol-training mb-0">
                         @foreach($group as $training)
                             @if($training->is_max === 1)
@@ -26,6 +26,5 @@
         </div>
     </div>
 </div>
-
 
 @endsection
