a3c77dae2d2f059155927e7ca92574383e1ebde8
白川聡 <kyabetsu3104@gmail.com>
2020-03-14 16:20:11 +0900

    viewをincludeで共通化

--- resources/views/event/show.blade.php
+++ resources/views/event/show.blade.php
@@ -26,74 +26,15 @@
     </div>
 
     <div class = "trainings-index  pb-4">
-
-        <div class = "today-trainings">
-            <a  href="/events/{{$thisEvent->event_id}}">
-                <div class = "event-title">{{$thisEvent->date}}の{{App\Defs\DefPart::PART_NAME_LIST[$thisEvent->part_code]}}トレ</div>
-            </a>
-            <div class="this-event-show" data-event_id= {{$thisEvent->event_id}}>
-                <span class = "event-memo">{{$thisEvent->getMemo()}}</span>
-                <div class = "this-trainings">
-                    @foreach($thisTrainings as $group)
-                        <div class = "card mt-2 mb-2 mr-2 ml-2 p-2">
-                            <span class = "mb-0">
-                                <a class = "card-title" href="/stages/{{$group[0]->stage_id}}">{{ $group[0]->getStageName() }}</a>
-                            </span>
-                            <ol data-stage_id = {{ $group[0]->stage_id }} class = "ol-training mb-0">
-                            @foreach($group as $training)
-                                @if($training->is_max == 1)
-                                    <li data-training_id = {{$training->training_id}} class = "this-training _add-underline ">{{$training->getWeightAndRep() }}</li>
-                                @else
-                                    <li data-training_id = {{$training->training_id}} class = "this-training ">{{$training->getWeightAndRep() }}</li>
-                                @endif
-                            @endforeach
-                            </ol>
-                        </div>
-                    @endforeach
-
-                </div>
-            </div>
-            <div>
-                <a href="/events/{{$thisEvent->event_id}}/destroy"><i class="fas fa-trash ml-2"></i></a>
-                <div class="float-right"><a  href={{url()->previous()}}><button>戻る</button></a></div>
-            </div>
-
-
+        @include('partials.event', ['event' => $thisEvent, 'trainings' => $thisTrainings, 'type' => 'this' ])
+        <div>
+            <a href="/events/{{$thisEvent->event_id}}/destroy"><i class="fas fa-trash ml-2"></i></a>
+            <div class="float-right"><a  href={{url()->previous()}}><button>戻る</button></a></div>
         </div>
 
-
-        @isset($lastEvent)
-
-            <div class = "last-trainings">
-                <a href="/events/{{$lastEvent->event_id}}">
-                    <div class = "event-title">{{$lastEvent->date}}の{{App\Defs\DefPart::PART_NAME_LIST[$lastEvent->part_code]}}トレ</div>
-                </a>
-                <div class="last-event-show" data-event_id= {{$lastEvent->event_id}}>
-                    <span class = "event-memo">{{$lastEvent->getMemo()}}</span>
-                    <div class = "last-trainings">
-                        @foreach($lastTrainings as $group)
-                            <div class = "card mt-2 mb-2 mr-2 ml-2 p-2">
-                                <span class = "mb-0">
-                                    <a class = "card-title" href="/stages/{{$group[0]->stage_id}}">{{ $group[0]->getStageName() }}</a>
-                                </span>
-                                <ol data-stage_id = {{ $group[0]->stage_id }} class = "ol-training mb-0">
-                                @foreach($group as $training)
-                                    @if($training->is_max == 1)
-                                        <li data-training_id = {{$training->training_id}} class = "last-training _add-underline ">{{$training->getWeightAndRep() }}</li>
-                                    @else
-                                        <li data-training_id = {{$training->training_id}} class = "last-training ">{{$training->getWeightAndRep() }}</li>
-                                    @endif
-                                @endforeach
-                                </ol>
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-
+        @isset($previousEvent)
+            @include('partials.event',['event'=>$previousEvent,'trainings'=>$previousTrainings,'type'=>'previous'])
         @endisset
-
-
     </div>
 </div>
 
---
+++ resources/views/partials/event.blade.php
@@ -0,0 +1,18 @@
+<a href="/events/{{$event->event_id}}">
+    <div class = "event-title">{{$event->date}}の{{App\Defs\DefPart::PART_NAME_LIST[$event->part_code]}}トレ</div>
+</a>
+<div class="{{$type}}-event-show" data-event_id= {{$event->event_id}}>
+    <span class = "event-memo">{{$event->getMemo()}}</span>
+    <div class = "{{$type}}-trainings">
+        @foreach($trainings as $group)
+            <div class = "card mt-2 mb-2 mr-2 ml-2 p-2">
+                <span class = "mb-0">
+                    <a class = "card-title" href="/stages/{{$group[0]->stage_id}}">{{ $group[0]->getStageName() }}</a>
+                </span>
+                <ol data-stage_id = {{ $group[0]->stage_id }} class = "ol-training mb-0">
+                    @include('partials.training', [ 'group' => $group, 'type' => $type ])
+                </ol>
+            </div>
+        @endforeach
+    </div>
+</div>
