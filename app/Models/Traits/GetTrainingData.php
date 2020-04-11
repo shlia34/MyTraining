5e9dc29ade5cf84a3a709717030cc09024bd28c2
白川聡 <kyabetsu3104@gmail.com>
2020-04-11 18:25:09 +0900

    アクセサを使ってみた

--- app/Models/Traits/GetTrainingData.php
+++ app/Models/Traits/GetTrainingData.php
@@ -6,13 +6,13 @@ use App\Models\Stage;
 
 trait GetTrainingData
 {
-    public function getStageName()
+    public function getStageNameAttribute()
     {
         return Stage::where("stage_id",$this->stage_id)->first()->name ?? "";
         //maxが登録されてない場合は空文字を返す
     }
 
-    public function getWeightAndRep(){
+    public function getWeightAndRepAttribute(){
         if( ($this->weight !== null) && ($this->rep !== null) ){
             return $this->weight."kg". " * " .$this->rep."rep";
         }else{
