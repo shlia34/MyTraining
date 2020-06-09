2b2a072d33edf86337994865e03aeef34d726b6c
shira <shira79@users.noreply.github.com>
2020-06-09 16:49:45 +0900

    api.phpに移動

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('/', 'ProgramController@index')->name('program.index');
         Route::get('/programs/{program_id}', 'ProgramController@show')->name('program.show');
         Route::get('/programs/{program_id}/destroy', 'ProgramController@destroy')->name('program.destroy');
-        //exrcise
+        //exercise
         Route::group(['prefix' => 'exercises'],function (){
             Route::get('/index', 'ExerciseController@index')->name('exercise.index');
             Route::get('/create', 'ExerciseController@create')->name('exercise.create');
@@ -32,35 +32,6 @@ Route::group(['middleware' => 'auth'], function () {
             Route::get('/index', 'CsvController@index')->name('csv.index');
             Route::get('/export/{modelName}', 'CsvController@export')->name('csv.export');
             Route::post('/import', 'CsvController@import')->name('csv.import');
-        });
-
-    });
-    //api
-    Route::group(['namespace' => 'Api','prefix' => 'api'], function () {
-        //program api
-        Route::group(['prefix' => 'programs'], function () {
-            Route::get('/set', 'ProgramController@set')->name('program.set');
-            Route::post('/showLinks', 'ProgramController@showLinks')->name('program.showLinks');
-            Route::post('/store', 'ProgramController@store')->name('program.store');
-            Route::post('/updateDate', 'ProgramController@updateDate')->name('program.updateDate');
-        });
-        //workout api
-        Route::group(['prefix' => 'workouts'], function () {
-            //workout
-            Route::post('/store', 'WorkoutController@store')->name('workout.store');
-            Route::post('/destroy', 'WorkoutController@destroy')->name('workout.destroy');
-            //is_max
-            Route::group(['prefix' => 'max'],function () {
-                Route::post('/on', 'WorkoutController@OnMax')->name('workout.onMax');
-                Route::post('/off', 'WorkoutController@OffMax')->name('workout.offMax');
-                Route::post('/check', 'WorkoutController@checkMax')->name('workout.checkMax');
-            });
-        });
-        //Routine api
-        Route::group(['prefix' => 'routines'],function (){
-            Route::post('/store', 'RoutineController@store')->name('routine.store');
-            Route::post('/destroy', 'RoutineController@destroy')->name('routine.destroy');
-            Route::post('/sort', 'RoutineController@sort')->name('routine.sort');
         });
 
     });
