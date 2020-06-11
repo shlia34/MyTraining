9e8bbea3f6f1d9f3d0d4cd5ebd3c83162ad71d80
白川聡 <shlia34@users.noreply.github.com>
2020-06-11 18:11:53 +0900

    アラート

--- public/js/program-control.js
+++ public/js/program-control.js
@@ -58,3 +58,15 @@ function showLinksProgram(date)
         });
     });
 }
+
+$(function()
+{
+    alertDestroyProgram();
+});
+
+function alertDestroyProgram() {
+    $(document).on("click", ".fa-trash", function () {
+        alert('本当に消しますか？？？');
+    });
+
+}
