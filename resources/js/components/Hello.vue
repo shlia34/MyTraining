eca8fde53ee09874b5df6bc761c559bcafef0fd0
shira <shira79@users.noreply.github.com>
2020-06-12 22:23:42 +0900

    vueセットアップ

---
+++ resources/js/components/Hello.vue
@@ -0,0 +1,14 @@
+<template>
+    <div><h2>Hello <span class="name">{{name}}</span></h2>!</div>
+</template>
+
+<script>
+    export default {
+        props: ['name']
+    }
+</script>
+<style scoped>
+    .name {
+        font-weight: bold;
+    }
+</style>
