a61d2d6689e0f5aa552a3d3d58df5f3e44bc37ef
shira <shira79@users.noreply.github.com>
2020-06-15 03:37:43 +0900

    modalのform完成

---
+++ resources/js/const.js
@@ -0,0 +1,11 @@
+export const muscleNames = [
+    { "id": '01', "name": '胸' },
+    { "id": '02', "name": '背中' },
+    { "id": '03', "name": '脚' },
+    { "id": '04', "name": '肩' },
+    { "id": '05', "name": '二頭筋' },
+    { "id": '06', "name": '二頭筋' },
+    { "id": '07', "name": '腹筋' },
+];
+
+//todo laravelのdefファイルと重複しちゃってる。
