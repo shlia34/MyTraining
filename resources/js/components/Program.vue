34cce353759efc6a7f7b38030c80e0521519bc6e
shira <shira79@users.noreply.github.com>
2020-06-15 20:42:06 +0900

    program.showを表示だけ

---
+++ resources/js/components/Program.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <a :href="'/programs/' + item.id" >
+            <div class = "program-title">{{item.date}}の{{item.muscle_name}}トレ</div>
+        </a>
+
+        <div :data-program_id =item.id class="program-show">
+                <span class = "program-memo">{{item.memo}}</span>
+                <div class = "workouts">
+                    <Menu
+                        :clickable="clickable"
+                        :key="menu.id"
+                        :menu="menu"
+                        v-for="menu in item.menus"
+                    ></Menu>
+                </div>
+            </div>
+    </div>
+</template>
+
+
+<script>
+    import Menu from './Menu.vue';
+
+    export default {
+        props:['item','text','clickable'],
+        components:{
+            Menu,
+        },
+        data: function(){
+            return{
+
+            }
+        }
+    }
+
+
+</script>
