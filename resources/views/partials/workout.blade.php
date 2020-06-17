bfc976701236b453229c1fdf3c5e12c5027fe66d
shira <shira79@users.noreply.github.com>
2020-06-17 20:35:16 +0900

    不要なファイル削除

--- resources/views/partials/workout.blade.php
+++
@@ -1,6 +0,0 @@
-@if($workout->is_max == 1)
-    <li data-workout_id = {{$workout->id}} class = "{{$type}}-workout _add-underline ">{{$workout->weight_and_rep }}</li>
-@else
-    <li data-workout_id = {{$workout->id}} class = "{{$type}}-workout">{{$workout->weight_and_rep }}</li>
-@endif
-{{-- event.showとstage.showで使ってる --}}
