d5825a2a596c7905498a10e499270b4c61b26fed
白川聡 <shlia34@users.noreply.github.com>
2020-06-23 01:21:03 +0900

    vuetify入れた

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,50 +1,52 @@
 <template>
 
+    <v-app id="inspire">
+
+    </v-app>
     <!--Navbar-->
-    <nav class="navbar fixed-top">
-        <!-- Navbar brand -->
-        <router-link class="navbar-brand" to="/" >MyTraining</router-link>
-        <!-- Collapse button -->
-        <button aria-controls="navbarSupportedContent23" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler second-button"
-                data-target="#navbarSupportedContent23" data-toggle="collapse" type="button">
-            <div class="animated-icon2"><span></span><span></span><span></span><span></span></div>
-        </button>
+<!--    <nav class="navbar fixed-top">-->
+<!--        &lt;!&ndash; Navbar brand &ndash;&gt;-->
+<!--        <router-link class="navbar-brand" to="/" >MyTraining</router-link>-->
+<!--        &lt;!&ndash; Collapse button &ndash;&gt;-->
+<!--        <button aria-controls="navbarSupportedContent23" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler second-button"-->
+<!--                data-target="#navbarSupportedContent23" data-toggle="collapse" type="button">-->
+<!--            <div class="animated-icon2"><span></span><span></span><span></span><span></span></div>-->
+<!--        </button>-->
 
-        <!-- Collapsible content -->
-        <div class="collapse navbar-collapse" id="navbarSupportedContent23">
-
-            <!-- Links -->
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <router-link class="nav-link" to="/exercises/index/01" ><i class="fas fa-cog"></i> 種目</router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link class="nav-link" to="/csv/index" ><i class="fas fa-file-csv"></i> CSV</router-link>
-                </li>
-                <li class="nav-item">
-                    <a @click="logout" class="nav-link">ログアウト</a>
-<!--                       onclick="event.preventDefault();-->
-<!--                       document.getElementById('logout-form').submit();"><i class="fas fa-sign-out-alt"></i> ログアウト</a>-->
-<!--                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">-->
-<!--                        @csrf-->
-<!--                    </form>-->
-                </li>
-            </ul>
-            <!-- Links -->
-        </div>
-        <!-- Collapsible content -->
-    </nav>
-    <!--/.Navbar-->
+<!--        &lt;!&ndash; Collapsible content &ndash;&gt;-->
+<!--        <div class="collapse navbar-collapse" id="navbarSupportedContent23">-->
+<!--            &lt;!&ndash; Links &ndash;&gt;-->
+<!--            <ul class="navbar-nav mr-auto">-->
+<!--                <li class="nav-item">-->
+<!--                    <router-link class="nav-link" to="/exercises/index/01" ><i class="fas fa-cog"></i> 種目</router-link>-->
+<!--                </li>-->
+<!--                <li class="nav-item">-->
+<!--                    <router-link class="nav-link" to="/csv/index" ><i class="fas fa-file-csv"></i> CSV</router-link>-->
+<!--                </li>-->
+<!--                <li class="nav-item">-->
+<!--                    <a @click="logout" class="nav-link">ログアウト</a>-->
+<!--                </li>-->
+<!--            </ul>-->
+<!--            &lt;!&ndash; Links &ndash;&gt;-->
+<!--        </div>-->
+<!--        &lt;!&ndash; Collapsible content &ndash;&gt;-->
+<!--    </nav>-->
+<!--    &lt;!&ndash;/.Navbar&ndash;&gt;-->
 
 </template>
 
 
 <script>
+
+    import {alertError} from '../alert'
+
     export default {
+        mixins:[alertError],
         methods:{
             logout:function(){
+                var vm = this;
                 var response = axios.post('/logout').then(function (response) {
-                    console.log("logouted");
+                    location.href = '/login';
                 })
                 .catch(function (error) {
                     vm.alertError(error.response);
