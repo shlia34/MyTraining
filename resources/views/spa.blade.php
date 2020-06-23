d7f1bd8c1a79a2a9439dfe8aa1e6959f14f98960
白川聡 <shlia34@users.noreply.github.com>
2020-06-23 21:16:47 +0900

    icon使えるようにした

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -21,6 +21,8 @@
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href='/css/style.css' type="text/css" rel='stylesheet' />
+{{--icon--}}
+        <link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css" rel="stylesheet">
     </head>
 
     <body>
